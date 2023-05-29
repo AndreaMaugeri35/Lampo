f8df83ad19770d8024a89655e10f6694cd8a73aa
Andrea Maugeri <andrewmaugeri@live.it>
2023-05-29 15:17:58 +0200

    modificata mail

--- resources/views/mail/become_revisor.blade.php
+++ resources/views/mail/become_revisor.blade.php
@@ -6,14 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vendilo.it</title>
 </head>
-<body>
-    <div>
-        <h1>Un utente ha richiesto di lavorare con noi</h1>
-        <h2>Ecco i suoi dati:</h2>
-        <p>Nome: {{ $user->name }}</p>
-        <p>Email: {{ $user->email }}</p>
-        <p>Se vuoi renderlo revisore clicca qui: </p>
-        <a href="{{ route('make.revisor', compact('user'))}}">Rendi revisore</a>
+<body style="background-color: #252525; display:flex; justify-content: center; font-size:large">
+    <div style=" display:flex; flex-direction:column; align-items: center;">
+        <h1 style="color: #A238FF;">Lampo.it</h1>
+        <h2 style="color: #A238FF">Un utente ha richiesto di lavorare con noi</h1>
+        <p style="color: white">Salve staff di Lampo.it, Vorrei entrare a far parte del team, nel ruolo di revisore</p>    
+        <h3 style="color: white">Ecco i miei dati:</h2>
+        <p style="color: white">Nome: {{ $user->name }}</p>
+        <p style="color: white">Email: {{ $user->email }}</p>
+        <p style="color: white">Utente dal: {{ $user->created_at->format('d/m/Y') }}</p>
+        <p style="color: white">Per rendere revisone questo utente clicca qui: </p>
+        <a style="color: #A238FF" href="{{ route('make.revisor', compact('user'))}}">Rendi revisore</a>
     </div>
 </body>
 </html>
