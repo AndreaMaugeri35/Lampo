b48f4d2214b162efa17e871aa0d403c342b8b2f1
Andrea Maugeri <andrewmaugeri@live.it>
2023-05-29 03:34:50 +0200

    aggiunta possibilità per un utente loggato di vedere gli annunci pubblicati, rrifiutati o da revisionare nella propria pagina profilo

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,7 +1,7 @@
 <x-layout title="Il tuo profilo: {{Auth::user()->name}}" header="Benvenuto nella tua area personale, {{Auth::user()->name}}">
     <div class="container my-5">
         <div class="row justify-content-center">
-            
+                <h2 class="text-white text-center">Annunci pubblicati</h2>
                 @forelse (Auth::user()->announcements->where('is_accepted', true) as $announcement)
                 <div class="col-12 col-md-3 my-5">
                     <div class="card">
@@ -18,12 +18,32 @@
                 </div>
                 @empty
                     <div class="col-12 my-5 d-flex flex-column align-items-center">
-                        <h2 class="text-center text-primaryC display-3 fw-bold">Non ci sono annunci</h2>
+                        <h2 class="text-center text-white display-3 fw-bold">Nessun annuncio pubblicato</h2>
                     </div>
                 @endforelse
                     <div class="col-12 my-5 d-flex flex-column align-items-center">               
                         <a href="{{route('announcement.create')}}" class="btn btnCategory text-white my-5">Pubblica un nuovo annuncio</a>
-                    </div>       
+                    </div> 
+                    <h2 class="text-white text-center">Annunci rifiutati o in attesa di revisione</h2>
+                    @forelse (Auth::user()->announcements->where('is_accepted',!1) as $announcement)
+                    <div class="col-12 col-md-3 my-5">
+                        <div class="card">
+                            <img src="https://picsum.photos/200" class="card-img-top" alt="...">
+                            
+                            <div class="card-body">
+                              <h5 class="card-title">{{$announcement->title}}</h5>
+                              <p class="card-text">{{$announcement->price}} €</p>
+                              <a href="{{route('categoryShow',['category'=>$announcement->category])}}" class="my-1 btn btn-primary btnCategory">Categoria: {{$announcement->category->name}}</a>
+                              <p class="card-footer">@if (is_numeric($announcement->is_accepted))Rifiutato @else In attesa di revisione @endif </p>
+                            </div>
+                        </div>
+                    </div>
+                    @empty
+                        <div class="col-12 my-5 d-flex flex-column align-items-center">
+                            <h2 class="text-center text-white display-3 fw-bold">Nessun annuncio rifiutato o in attesa di revisione</h2>
+                        </div>
+                    @endforelse    
+                    
         </div>
     </div>
 </x-layout>
