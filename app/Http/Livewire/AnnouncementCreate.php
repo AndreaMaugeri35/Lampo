9f92c589bec9c331e0e878e8dd53c440390aed85
Pasquale Macera <paky98@live.it>
2023-06-01 11:06:55 +0200

    Applicata Immagine di censura al volto

--- app/Http/Livewire/AnnouncementCreate.php
+++ app/Http/Livewire/AnnouncementCreate.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use App\Models\Announcement;
 use Livewire\WithFileUploads;
@@ -77,9 +78,13 @@ class AnnouncementCreate extends Component
                     // $this->announcement->images()->create(['path'=>$image->store('images', 'public')]);
                     $newFileName="announcements/{$this->announcement->id}";
                     $newImage=$this->announcement->images()->create(['path'=>$image->store($newFileName,'public')]);
-                    dispatch(new ResizeImage($newImage->path,300,200));
-                    dispatch(new GoogleVisionSafeSearch($newImage->id));
-                    dispatch(new GoogleVisionLabelImage($newImage->id));
+                    RemoveFaces::withChain([
+                       new ResizeImage($newImage->path,300,200),
+                       new GoogleVisionSafeSearch($newImage->id),
+                       new GoogleVisionLabelImage($newImage->id)
+
+                    ])->dispatch($newImage->id);
+                   
                 }
                 File::deleteDirectory(storage_path('/app/livewire-tmp'));
             }
